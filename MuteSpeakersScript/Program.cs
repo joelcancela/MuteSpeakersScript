02b7a6d8d85b6cb08992680018beca86488236a1
Joël CANCELA VAZ <joel.cancelavaz@gmail.com>
2018-08-26 22:05:33 +0200

    Script working

---
+++ MuteSpeakersScript/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NAudio.CoreAudioApi;
+
+namespace MuteSpeakersScript
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                MMDeviceEnumerator MMDE = new NAudio.CoreAudioApi.MMDeviceEnumerator();
+                MMDeviceCollection DevCol = MMDE.EnumerateAudioEndPoints(NAudio.CoreAudioApi.DataFlow.All, NAudio.CoreAudioApi.DeviceState.All);
+                foreach (NAudio.CoreAudioApi.MMDevice dev in DevCol)
+                {
+                    try
+                    {
+                        if (dev.FriendlyName.Contains("Haut-parleurs"))
+                        {
+                            dev.AudioEndpointVolume.Mute = true;
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        // ignored
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                // ignored
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # MuteSpeakersScript
 
+Just a simple script to mute the speakers (works only for French Windows)
